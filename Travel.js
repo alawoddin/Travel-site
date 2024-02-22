ba268e0850c58759634275ab674c2169c42f3473
Allahuddin <amk17th@AMKs-MacBook-Air.local>
2024-02-22 22:51:36 +0430

    the travel site for class task

---
+++ Travel.js
@@ -0,0 +1,122 @@
+document.getElementById('container').style.width = '100%';
+document.getElementById('container').style.height = '500vh';
+document.getElementById('container').style.backgroundColor = 'red'
+
+document.getElementById('box1').style.width = '100%';
+document.getElementById('box1').style.height = '40px';
+document.getElementById('box1').style.backgroundColor = 'green';
+document.getElementById('box1').style.display = 'flex';
+document.getElementById('box1').style.gap = '10px';
+
+//header section 
+
+document.getElementById('header').style.width = '100%';
+document.getElementById('header').style.height = '60px';
+document.getElementById('header').style.backgroundColor = 'wheat';
+
+//iamge section 
+
+document.getElementById('image').style.width = '100%'
+document.getElementById('image').style.height = '400px';
+document.getElementById('image').style.backgroundColor = 'blue';
+
+//colume data
+
+document.getElementById('Data').style.width = '100.8%'
+document.getElementById('Data').style.height = '200px'
+document.getElementById('Data').style.backgroundColor = 'orange'
+
+//time for  
+
+document.getElementById('time').style.width = '100%'
+document.getElementById('time').style.height = '40px'
+document.getElementById('time').style.backgroundColor = 'gray'
+
+//day 
+
+document.getElementById('day').style.width = '100%'
+document.getElementById('day').style.height = '40px'
+document.getElementById('day').style.backgroundColor = 'blue'
+document.getElementById('day').style.display = 'flex'
+document.getElementById('day').style.gap = '10px'
+
+//section body 
+
+document.getElementById('body').style.width = '100%';
+document.getElementById('body').style.height = '500px';
+document.getElementById('body').style.backgroundColor = 'gold';
+document.getElementById('body').style.display = 'flex';
+
+
+//information section 
+
+document.getElementById('info').style.width = '100%'
+document.getElementById('info').style.height = '790px';
+document.getElementById('info').style.backgroundColor = 'blue'
+
+
+// it is blog to ticket 
+
+document.getElementById('ticket').style.width = '90%'
+document.getElementById('ticket').style.height = '590px'
+document.getElementById('ticket').style.backgroundColor = 'orange'
+
+//col-6 form ticket 
+
+document.getElementById('imgs').style.width = '50%'
+document.getElementById('imgs').style.height = '290px'
+document.getElementById('imgs').style.backgroundColor = 'gray'
+
+//text 
+
+document.getElementById('text').style.width = '50%';
+document.getElementById('text').style.height = '290px';
+document.getElementById('text').style.backgroundColor = 'pink'
+
+
+//second col- 6 information 
+
+
+document.getElementById('imgs1').style.width = '50%'
+document.getElementById('imgs1').style.height = '290px'
+document.getElementById('imgs1').style.backgroundColor = 'gray'
+
+
+
+document.getElementById('text1').style.width = '50%';
+document.getElementById('text1').style.height = '290px';
+document.getElementById('text1').style.backgroundColor = 'pink'
+
+//
+
+document.getElementById('imgs2').style.width = '50%'
+document.getElementById('imgs2').style.height = '290px'
+document.getElementById('imgs2').style.backgroundColor = 'gray'
+
+document.getElementById('text2').style.width = '50%';
+document.getElementById('text2').style.height = '290px';
+document.getElementById('text2').style.backgroundColor = 'pink'
+
+//
+
+document.getElementById('imgs3').style.width = '50%'
+document.getElementById('imgs3').style.height = '290px'
+document.getElementById('imgs3').style.backgroundColor = 'gray'
+
+document.getElementById('text3').style.width = '50%';
+document.getElementById('text3').style.height = '290px';
+document.getElementById('text3').style.backgroundColor = 'pink'
+
+
+//second last section near 
+
+document.getElementById('near').style.width = '90%';
+document.getElementById('near').style.height = '500px';
+document.getElementById('near').style.backgroundColor = 'pink'
+document.getElementById('near').style.margin = '0px auto'
+
+// footer section 
+
+document.getElementById('footer').style.width = '100%'
+document.getElementById('footer').style.height = '500px';
+document.getElementById('footer').style.backgroundColor = 'yellow'
